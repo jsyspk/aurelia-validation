4013e7e5e62512ab624c05c71dd54cd91e706775
Ali Masood <ahmad@zenex.co>
2019-07-11 17:53:05 +0900

    Adds validation with message from the main object

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { inject } from 'aurelia-framework';
+import Project from "./project";
+
+@inject(Project)
 export class App {
-  public message: string = 'Hello World!';
+
+  public message: string = 'Ho to use validation with simple javascript objects';
+  private project;
+
+  constructor(project: Project){
+    this.project = project;
+  }
 }
